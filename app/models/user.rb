566a35f6c19fe036b1e37201331c3beb375acafb
Kara <kara.a.flaherty@gmail.com>
2017-08-06 15:05:27 -0600

    Added username to users, gave users ability to sign in using either username or password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  # Allow username or UserName
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  # Only allow letter, number, underscore and punctuation -- NOT @ -- to avoid emails as usernames
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  # Change the behavior of the login action to take either usernme or email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
 end
