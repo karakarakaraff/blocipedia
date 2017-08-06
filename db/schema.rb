a789650ede47f0d2068735e3212c78348a3f1c1f
Kara <kara.a.flaherty@gmail.com>
2017-08-06 17:10:56 -0600

    Database settings for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20170806202344) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
-    t.index [nil], name: "index_users_on_unlock_token", unique: true
   end
 
 end
