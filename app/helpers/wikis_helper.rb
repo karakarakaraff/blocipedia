d9768228d1ba4dbc7bfafdac3f7e25865f64f1bb
Kara <kara.a.flaherty@gmail.com>
2017-08-11 00:28:12 -0600

    Added markdown formatting capabilities

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -22,4 +22,18 @@ module WikisHelper
       wiki.title
     end
   end
+
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      autolink: true,
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      underline: true,
+      highlight: true,
+      footnotes: true,
+      tables: true
+    })
+    @markdown.render(content)
+  end
+
 end
