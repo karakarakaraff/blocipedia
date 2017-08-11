71e0c5fe7b04feb2fcbaeac34ada14cd5a58ed47
Kara <kara.a.flaherty@gmail.com>
2017-08-10 23:09:57 -0600

    Changed database so wiki privacy default is false instead of nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809075307) do
+ActiveRecord::Schema.define(version: 20170811050247) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20170809075307) do
   create_table "wikis", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "private"
+    t.boolean "private", default: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
